460f2fd46aff4dd8d9e39e885c3cae5d4044a565
Anthone Silva <anthonej@gmail.com>
2018-09-30 19:56:59 -0300

    Testando adicionar automaticamente @author e @link

---
+++ javabasico/src/com/anthone/javabasico/lista03/Exercicio06.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ * @author Anthone Silva {@link anthonej@gmail.com}
+ *
+ * 30 de set de 2018
+ *
+ */
+package com.anthone.javabasico.lista03;
+
+/**
+ * @author Anthone Silva {@link anthonej@gmail.com}
+ *
+ */
+public class Exercicio06 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
