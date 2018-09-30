f1e41bf7caec6680c1746f2fb7bd90826564a623
Anthone Silva <anthonej@gmail.com>
2018-09-30 20:48:52 -0300

    Lógica do loop do maior número alterada

--- javabasico/src/com/anthone/javabasico/lista03/Exercicio07.java
+++ javabasico/src/com/anthone/javabasico/lista03/Exercicio07.java
@@ -22,32 +22,22 @@ public class Exercicio07 {
 
 		Scanner scan = new Scanner(System.in);
 		
-		System.out.println("MAIOR NÚMERO\nDigite 5 números, separados por espaço e pressione [ENTER]: ");
+		System.out.println("MAIOR NÚMERO");
 		
-		double num1 = scan.nextDouble();
-		double num2 = scan.nextDouble();
-		double num3 = scan.nextDouble();
-		double num4 = scan.nextDouble();
-		double num5 = scan.nextDouble();
+		double numero;
+		double maiorNumero = Double.MIN_VALUE;
 		
-		double maior = 0;
-		
-		do {
-			if (num1 > num2 && num1 > num3 && num1 > num4 && num1 > num5) {
-				maior = num1;
-			} else if (num2 > num1 && num2 > num3 && num2 > num4 && num2 > num5) {
-				maior = num2;
-			} else if (num3 > num1 && num3 > num2 && num3 > num4 && num3 > num5) {
-				maior = num3;
-			} else if (num4 > num1 && num4 > num2 && num4 > num3 && num4 > num5) {
-				maior = num4;
-			} else {
-				maior = num5;
-			}
+		for (int i=1; i<=5; i++) {
 			
-		} while (maior < 0);
+			System.out.println("Digite o " + i + "º número: ");
+			numero = scan.nextDouble();
+			
+			if (numero > maiorNumero) {
+				maiorNumero = numero;
+			}
+		}
 		
-		System.out.println("O número maior é o: " + maior);
+		System.out.println("\nO número maior é o: " + maiorNumero);
 		
 		scan.close();
 		
