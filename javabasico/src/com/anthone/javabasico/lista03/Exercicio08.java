14aaa41ba9a64fffa673c6636ae3864823d65da6
Anthone Silva <anthonej@gmail.com>
2018-09-30 20:54:56 -0300

    Exercicio08

---
+++ javabasico/src/com/anthone/javabasico/lista03/Exercicio08.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ * @author Anthone Silva
+ *
+ * 30 de set de 2018
+ *
+ */
+package com.anthone.javabasico.lista03;
+
+import java.util.Scanner;
+
+/**
+ * @author Anthone Silva
+ *
+ */
+public class Exercicio08 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("SOMANDO NÚMEROS E CALCULANDO A MÉDIA");
+		
+		Scanner scan = new Scanner(System.in);
+		
+		double numero;
+		double soma = 0;
+		double media;
+		
+		for (int i=1; i<=5; i++) {
+			
+			System.out.println("Digite o " + i + "º número: ");
+			numero = scan.nextDouble();
+			
+			soma += numero;
+			
+		}
+		
+		media = soma/5;
+		
+		System.out.println("\nA soma dos números é: " + Math.round(soma));
+		System.out.println("A média dos números é: " + Math.round(media));
+		
+		scan.close();
+
+	}
+
+}
