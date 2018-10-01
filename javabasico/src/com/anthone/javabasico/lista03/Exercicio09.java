ea753db0a27e2ec0e67c0e8d73949cf16b905b24
Anthone Silva <anthonej@gmail.com>
2018-09-30 21:09:58 -0300

    Exercicio09

---
+++ javabasico/src/com/anthone/javabasico/lista03/Exercicio09.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ * @author Anthone Silva
+ *
+ * 30 de set de 2018
+ *
+ */
+package com.anthone.javabasico.lista03;
+
+import java.util.Scanner;
+
+/**
+ * @author Anthone Silva
+ *
+ */
+public class Exercicio09 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("IMPRIMINDO NÚMEROS ÍMPARES ENTRE 1 E 50\n");
+		
+		for (int i=0; i<=50; i++) {
+			
+			if (i % 2 != 0) {
+				System.out.print(i + " ");
+			}
+			
+		}
+	}
+
+}
