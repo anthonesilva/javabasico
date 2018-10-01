1ba7bba58a93f3a2d23a12c41389a2bcd4a91792
Anthone Silva <anthonej@gmail.com>
2018-09-30 21:22:47 -0300

    Exercícios 10 e 11

---
+++ javabasico/src/com/anthone/javabasico/lista03/Exercicio11.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ * @author Anthone Silva
+ *
+ * 30 de set de 2018
+ *
+ */
+package com.anthone.javabasico.lista03;
+
+import java.util.Scanner;
+
+/**
+ * @author Anthone Silva
+ *
+ */
+public class Exercicio11 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("IMPRIMINDO NÚMEROS INTEIROS DE UM INTERVALO E SOMANDO-OS\n");
+		
+		double num1, num2, inteiros, soma = 0;		
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Digite o 1º número: ");
+		num1 = scan.nextDouble();
+		
+		System.out.println("Digite o 2º número: ");
+		num2 = scan.nextDouble();
+		
+		for (; num1<=num2; num1++) {
+			
+			if (num1 % 2 == 0 || num1 % 2 != 0) {
+				
+				inteiros = num1;
+				soma += inteiros;
+				System.out.println(inteiros);	
+			
+			}
+
+		}		
+		
+		System.out.println("A soma dos números é: " + soma);
+		
+		scan.close();
+		
+	}
+
+}
