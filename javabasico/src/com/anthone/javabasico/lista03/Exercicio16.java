d0dc452d2bf6957910895f7c9c7be2f7ca3a76d3
Anthone Silva <anthone.silva@saude.go.gov.br>
2018-10-01 11:30:08 -0300

    Sequência de Fibonacci até 500

---
+++ javabasico/src/com/anthone/javabasico/lista03/Exercicio16.java
@@ -0,0 +1,41 @@
+/**
+ * 
+ * @author Anthone Silva
+ * 
+ * 1 de out de 2018
+ * 
+ */
+package com.anthone.javabasico.lista03;
+
+import java.util.Scanner;
+
+/**
+ * @author Anthone Silva
+ *
+ */
+public class Exercicio16 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("SEQUÊNCIA DE FIBONACCI ATÉ 500\n");
+
+		double enesimoTermo, termoNmenos1 = 0, termoNmenos2 = 1, resultado = 0;
+		
+		System.out.println(Math.round(termoNmenos1));
+		System.out.println(Math.round(termoNmenos2));
+
+		for (int i = 1; resultado <= 500; i++) {
+
+			resultado = termoNmenos1 + termoNmenos2;
+			termoNmenos1 = termoNmenos2;
+			termoNmenos2 = resultado;
+			System.out.println(Math.round(resultado));
+
+		}
+
+	}
+
+}
